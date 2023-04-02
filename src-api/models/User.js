d1db5a472cd42d6b1af142deb17a4a96d63e2869
DanielVerano <alum.dveranom.2021@iesalixar.org>
2023-04-02 20:56:10 +0200

    Modelo User

---
+++ src-api/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Introduzca su nombre'],
+        minlength: 3,
+        maxlength: 50
+    },
+    surname: {
+        type: String,
+        required: [true, 'Introduzca sus apellidos'],
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: [true, 'Introduzca su email'],
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Introduzca un email válido'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Introduzca una contraseña'],
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    avatar: {
+        type: String,
+        default: ''
+    }
+});
+
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+}
+
+module.exports = mongoose.model('User', UserSchema);
