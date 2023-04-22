d01dfddb01cd1d88a7478b134dd3026f2e09486a
DanielVerano <alum.dveranom.2021@iesalixar.org>
2023-04-22 12:09:23 +0200

    Vista de Productos

--- src-frontend/src/pages/ProductsPage.js
+++ src-frontend/src/pages/ProductsPage.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Breadcrumb, Filters, ProductList } from "../components";
 
 const ProductsPage = () => {
     return (
-        <div>ProductsPage</div>
+        <main>
+            <Breadcrumb title='Products' />
+            <Wrapper className='min-vh-100'>
+                <div className='d-flex w-100 my-3 mx-auto'>
+                    <div className="col-lg-3" >
+                        <Filters />
+                    </div>
+                    <div className="col-lg-9">
+                        <ProductList />
+                    </div>
+                </div>
+            </Wrapper>
+        </main>
     )
 }
+
+const Wrapper = styled.div`
+    
+`
 
 export default ProductsPage
