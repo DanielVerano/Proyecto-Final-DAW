a0ed7e7de1695b26e10b76ac06158ca37989e47f
DanielVerano <alum.dveranom.2021@iesalixar.org>
2023-04-23 12:39:34 +0200

    Modificacion Breadcrumb

--- src-frontend/src/components/Breadcrumb.js
+++ src-frontend/src/components/Breadcrumb.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Breadcrumb = ({ title }) => {
+const Breadcrumb = ({ title, productName }) => {
   return (
     <Wrapper className='d-flex align-items-center py-2 px-3' aria-label="breadcrumb">
       <ol className="breadcrumb mb-0">
         <li className="breadcrumb-item">Home</li>
         {title && <li className="breadcrumb-item active" aria-current="page">{title}</li>}
+        {productName && <li className="breadcrumb-item">{productName}</li>}
       </ol>
     </Wrapper>
   )
