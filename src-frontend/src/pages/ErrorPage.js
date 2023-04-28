0acec9c68f8d4c48b4efcd3d371b3998d7852fb3
DanielVerano <alum.dveranom.2021@iesalixar.org>
2023-04-28 12:46:14 +0200

    Vistas Error y Protected

--- src-frontend/src/pages/ErrorPage.js
+++ src-frontend/src/pages/ErrorPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>ErrorPage</div>
+        <Wrapper className='d-flex justify-content-center align-items-center'>
+            <div className='text-center'>
+                <h2>Error 404</h2>
+                <p>Página no encontrada</p>
+                <Link to='/' className='btn btn-primary'>Volver al inicio</Link>
+            </div>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    min-height: calc(100vh - (10vh + 8rem));
+`
 
 export default ErrorPage
--- src-frontend/src/pages/ProtectedRoute.js
+++ src-frontend/src/pages/ProtectedRoute.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { useUserContext } from "../context/user_context";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
     const { myUser } = useUserContext();
 
     if (!myUser) {
         return (
-            <div>ProtectedRoute</div>
+            <Wrapper className='d-flex justify-content-center align-items-center'>
+                <div className='text-center'>
+                    <h2>No tiene permiso para acceder a esta ruta.</h2>
+                    <Link to='/' className='btn btn-primary'>Volver al inicio</Link>
+                </div>
+            </Wrapper>
         )
     }
 
     return children;
 }
+
+const Wrapper = styled.div`
+    min-height: calc(100vh - (10vh + 8rem));
+`
 
 export default ProtectedRoute
